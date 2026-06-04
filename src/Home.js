de2aeb8b763d797ef0a7c8572360f73e43bda57d
Owen Zeng <owenzeng315@gmail.com>
2026-06-03 20:26:56 -0400

    Added hook for changing blog state and 3 simple blogs with some styling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'Avengers: Endgame', body: 'The final battle of the Infinity Saga.', author: 'Anthony Russo', id: 1 },
+        { title: 'Avengers: Infinity War', body: 'The ultimate threat to the universe.', author: 'Anthony Russo', id: 2 },
+        { title: 'Avengers: Age of Ultron', body: 'A new threat emerges from the past.', author: 'Joss Whedon', id: 3 }
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+
+            ))}
         </div>
     );
 }
