a99cd3c510630d5d6a8aa8b594f0c8d43382648b
Owen Zeng <owenzeng315@gmail.com>
2026-06-08 18:34:20 -0400

    Added a bloglist component for adding blogs and implemented a json database to fetch data

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'Avengers: Endgame', body: 'The final battle of the Infinity Saga.', author: 'Anthony Russo', id: 1 },
-        { title: 'Avengers: Infinity War', body: 'The ultimate threat to the universe.', author: 'Anthony Russo', id: 2 },
-        { title: 'Avengers: Age of Ultron', body: 'A new threat emerges from the past.', author: 'Joss Whedon', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null)
+
+    const [name, setName] = useState('Anthony Russo');
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data)
+            });
+    }, []);
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-
-            ))}
+            {blogs && <BlogList blogs={blogs} title = "All Blogs" />}
+            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'Anthony Russo')} title = "Anthony Russo's Blogs"/>}
+            <button onClick={() => setName('Joss Whedon')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
