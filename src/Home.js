3d41a8c04ad19eaf66f5f425765ed261ab8be59e
Owen Zeng <owenzeng315@gmail.com>
2026-06-10 18:30:27 -0400

    Added custom hook useFetch + cleaned up website functions
    useFetch handles data loading with error handling and also a loading message. Removed unnecessary things displayed on the website.

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,17 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    const [name, setName] = useState('Anthony Russo');
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data)
-            });
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title = "All Blogs" />}
             {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'Anthony Russo')} title = "Anthony Russo's Blogs"/>}
-            <button onClick={() => setName('Joss Whedon')}>Change Name</button>
-            <p>{ name }</p>
         </div>
     );
 }
- 
+
 export default Home;
