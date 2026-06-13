2969edf05b82ff671be9667687ec873b8f9bf640
Owen Zeng <owenzeng315@gmail.com>
2026-06-13 15:58:31 -0400

    Updated fetch links to hosted cloud

--- src/Create.js
+++ src/Create.js
@@ -14,7 +14,7 @@ const Create = () => {
         
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs', {
+        fetch('https://thanosblog.onrender.com/blogs', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(blog)
